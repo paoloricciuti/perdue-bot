8fa9baaaa7146bec0906f8321ce5640881a4222c
Paolo Ricciuti <ricciutipaolo@gmail.com>
2021-03-17 15:27:16 +0100

    Add upsert for team command

--- commands/team.js
+++ commands/team.js
@@ -1,19 +1,26 @@
-const utils=require('../utils');
+const utils = require('../utils');
 
-const exec=async (update, db)=>{
-    const capitani=await db.get("perdue-capitani");
-    const [command, ...args]=update.message.text.split(" ");
-    const team=args.join(" ");
-    const toInsert=update.message.from;
-    toInsert.team=team;
-    capitani.insert(toInsert);
-    const message={
+const exec = async (update, db) => {
+    const capitani = await db.get("perdue-capitani");
+    const [command, ...args] = update.message.text.split(" ");
+    const team = args.join(" ");
+    const toInsert = update.message.from;
+    toInsert.team = team;
+    capitani.findOneAndUpdate({
+        id: toInsert.id
+    }, {
+        $set: toInsert
+    }, {
+        upsert: true,
+        new: true
+    });
+    const message = {
         chat_id: update.message.chat.id,
-        text: "Perfetto, ti ho aggiunto come capitano della squadra "+team
+        text: "Perfetto, ti ho aggiunto come capitano della squadra " + team
     }
     utils.sendMessage(message)
 }
 
-module.exports={
+module.exports = {
     exec
 }
