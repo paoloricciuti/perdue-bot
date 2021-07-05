40d6cea6f207181c5908bf11428609b4b4571cf8
Paolo Ricciuti <ricciutipaolo@gmail.com>
2021-07-05 18:22:31 +0200

    Added aptfile and tryat

--- index.js
+++ index.js
@@ -1,38 +1,49 @@
 const express = require('express');
-const http= require('http');
-const utils= require('./utils');
+const http = require('http');
+const utils = require('./utils');
+const { exec } = require("child_process");
 require('dotenv').config()
 let db;
-try{
+try {
     db = require('monk')(process.env.MONGO_CONNECTION_STRING);
-}catch(e){
+} catch (e) {
     console.log(e);
 }
-const app=express();
+const app = express();
 app.use(express.json());
-const server=http.createServer(app);
+const server = http.createServer(app);
 
-app.get(`/${process.env.BOT_TOKEN}`, (req, res)=>{
+app.get(`/${process.env.BOT_TOKEN}`, (req, res) => {
     res.json({
         active: true
     })
 })
 
-app.get("/keepalive", (req, res)=>{
+app.get("/keepalive", (req, res) => {
     res.sendStatus(200);
 })
 
-app.post(`/${process.env.BOT_TOKEN}`, (req, res)=>{
+app.get("/tryat", (req, res) => {
+    exec("echo node crons/cron.js | at now +1 minutes", (err, stdout, stderr) => {
+        res.json({
+            err,
+            stdout,
+            stderr,
+        })
+    })
+})
+
+app.post(`/${process.env.BOT_TOKEN}`, (req, res) => {
     const { body: update } = req;
-    const commandString=utils.getCommand(update);
+    const commandString = utils.getCommand(update);
     console.log(commandString)
-    try{
-        const command=require(`./commands${commandString}`);
+    try {
+        const command = require(`./commands${commandString}`);
         command.exec(update, db);
-    }catch(e){}
+    } catch (e) { }
     res.sendStatus(200);
 });
 
-server.listen(process.env.PORT || 3000, ()=>{
+server.listen(process.env.PORT || 3000, () => {
     console.log("Server up and running...");
 })
